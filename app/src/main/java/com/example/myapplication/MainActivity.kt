3fd04756bb485438dcb60a37407459b91c938b32
Sianek <90836196+MikolajLeszczynski15@users.noreply.github.com>
2022-02-03 13:54:04 +0100

    1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupAutoCompleteTextView()
+    }
+    private fun setupAutoCompleteTextView() {
+        val countries = resources.getStringArray(R.array.panstewka)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+        binding.autoCompleteTextView.setAdapter(adapter)
+
+        binding.btnSubmit.setOnClickListener {
+            val text = getString(R.string.tekkst) + " " + binding.autoCompleteTextView.text
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
